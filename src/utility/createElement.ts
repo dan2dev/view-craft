743faa1299eae98084a2f2c9589386d424c3146a
Danilo Castro <dan2dev@outlook.com>
2025-06-21 20:02:12 -0300

    updates

--- src/utility/createElement.ts
+++ src/utility/createElement.ts
@@ -1,11 +1,24 @@
-export type VHtmlElement<TTagName extends keyof HTMLElementTagNameMap> = {
-  tagName: TTagName;
+import { isBrowser, isNode } from "./isBrowser";
+
+export type TVHtmlElement<TTagName extends keyof HTMLElementTagNameMap> = {
   render?: () => string;
   toString: () => string;
-} & Omit<
-  Partial<HTMLElementTagNameMap[TTagName]>,
-  "tagName" | "render" | "toString"
->;
+} & Omit<Partial<HTMLElementTagNameMap[TTagName]>, "render" | "toString">;
+
+export class VirtualHTMLElement<TTagName extends keyof HTMLElementTagNameMap> {
+  public readonly tagName: string;
+  constructor(tagName: TTagName) {
+    this.tagName = tagName as string;
+  }
+
+  public render(): string {
+    return `<${this.tagName}>`;
+  }
+
+  public toString(): string {
+    return `<${this.tagName}>`;
+  }
+}
 
 export type VTextNode = {
   textContent: string;
@@ -13,13 +26,17 @@ export type VTextNode = {
   toString: () => string;
 } & Omit<Partial<Text>, "textContent" | "render" | "toString">;
 
-export const createElement = <TTagName extends string>(
-  tagName: TTagName,
-): VHtmlElement<TTagName, HTMLElement> => {
-  if (document && document.createElement) {
-    return document.createElement(tagName, { is: tagName });
-  }
-};
+export const createElement = isBrowser
+  ? <TTagName extends keyof HTMLElementTagNameMap>(
+      tagName: TTagName,
+    ): TVHtmlElement<TTagName> => {
+      return document.createElement(tagName) as TVHtmlElement<TTagName>;
+    }
+  : <TTagName extends keyof HTMLElementTagNameMap>(
+      tagName: TTagName,
+    ): TVHtmlElement<TTagName> => {
+      return new VirtualHTMLElement(tagName) as TVHtmlElement<TTagName>;
+    };
 
 export const createTextNode = (text: string): VTextNode => {
   if (document && document.createTextNode) {
