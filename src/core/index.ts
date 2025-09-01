3af52483470439cd91232d21c2e5fd65f288968b
Danilo Castro <dan2dev@outlook.com>
2025-09-01 01:03:52 -0300

    feat: implement virtual element creation function and refine type definitions

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,11 @@
 // export namespace Core {
 //   register:
 // }
+
+export const v = <TTagName extends ElementTagName>(tagName: TTagName, attributes: ElementAttributes<TTagName>, children: (VirtualElement | Primitive)[]) => {
+  return {
+    tagName,
+    attributes,
+    children,
+  } satisfies VirtualElement<TTagName>;
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,58 +1,22 @@
-// Type definitions for view-craft
-// Project: view-craft
-// Definitions by: Your Name <your.email@example.com>
+import type { Primitive } from "type-fest/source/primitive";
 
-// import { Merge } from "type-fest";
-import { Merge, Or } from "type-fest";
-
-// Export your main types here. Example:
-
-// Element, SelfClosingElement, Text, Comment
 declare global {
   export type ElementTagName = keyof HTMLElementTagNameMap;
   export type ElementAttributes<TTagName extends ElementTagName = ElementTagName> = {
     [K in keyof HTMLElementTagNameMap[TTagName]]?: HTMLElementTagNameMap[TTagName][K] | (() => HTMLElementTagNameMap[TTagName][K]);
   }
-
-
-  // export type VirtualElementBase<TTagName extends ElementTagName = ElementTagName> = {
-  //   virtual?: {
-  //     tagName: TTagName;
-  //     attributes?: ElementAttributes<TTagName>;
-  //     nodes?: VirtualElement[];
-  //   };
-  // };
-  export type VirtualElementNode<TTagName extends ElementTagName = ElementTagName> = {
-    virtual: true;
-    tagName: string | TTagName;
+  export type VirtualElement<TTagName extends ElementTagName = ElementTagName> = {
+    tagName: TTagName;
     attributes: ElementAttributes<TTagName>;
-    coisa: string;
+    children: (VirtualElement | Primitive)[];
+    element?: HTMLElementTagNameMap[TTagName];
   }
-  export type VirtualElement<TTagName extends ElementTagName = ElementTagName> =
-    (HTMLElementTagNameMap[TTagName] & Partial<VirtualElementNode<TTagName>>) |
-    (VirtualElementNode<TTagName> & {
-      virtual: true;
-    });
 
-  // Merge<{
-  //   virtual: true,
-  //   tagName: TTagName;
-  //   attributes: ElementAttributes<TTagName>;
-  //   children: VirtualElement[];
-  // }, Partial<HTMLElementTagNameMap[TTagName]>>;
-
-
-  // export type VirtualElement<TTagName extends ElementTagName = ElementTagName> = {
-  //   // tagName: TTagName | string; // tag name
-  //   // att?: Partial<HTMLElementTagNameMap[TTagName]>;
-  //   // children?: VirtualElement[];
-  // } & OmitDeep<Partial<HTMLElementTagNameMap[TTagName]>, "children">;
-  /**
-   * A function that builds a virtual element tag.
-   */
-  export type NodeMod<TParentTag extends ElementTagName = ElementTagName> = (parent: VirtualElement<TParentTag>, index: number) => VirtualElement;
+  export type NodeMod<TParentTag extends ElementTagName = ElementTagName> =
+    (parent: VirtualElement<TParentTag>, index: number) =>
+      VirtualElement | Primitive
   export type NodeBuilder = (...children: NodeMod[]) => NodeMod;
 }
 
-// Add more types as needed for your API
+// Add more types as neeed for your API
 export { };
