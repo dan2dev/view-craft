f6da297c936032f1a23fd5c96ac358e60abdbaeb
Danilo Castro <dan2dev@outlook.com>
2025-09-09 00:36:02 -0300

    refactor: improve formatting and structure of type definitions for clarity

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,35 +6,36 @@ declare global {
 
   // Expanded element types (with mods)
   export type ExpandedElementAttributes<TTagName extends ElementTagName = ElementTagName> = {
-    [AttributeName in keyof HTMLElementTagNameMap[TTagName]]?:
-    | HTMLElementTagNameMap[TTagName][AttributeName]
-    | (() => HTMLElementTagNameMap[TTagName][AttributeName]);
+    [K in keyof HTMLElementTagNameMap[TTagName]]?:
+      | HTMLElementTagNameMap[TTagName][K]
+      | (() => HTMLElementTagNameMap[TTagName][K]);
   };
-  export type ExpandedElement<TTagName extends ElementTagName = ElementTagName> =
-    & Partial<Omit<HTMLElementTagNameMap[TTagName], 'tagName'>>
-    & Pick<HTMLElementTagNameMap[TTagName], 'tagName'>
-    & {
+  export type ExpandedElement<TTagName extends ElementTagName = ElementTagName> = Partial<
+    Omit<HTMLElementTagNameMap[TTagName], "tagName">
+  > &
+    Pick<HTMLElementTagNameMap[TTagName], "tagName"> & {
       rawMods?: (NodeMod<TTagName> | NodeModFn<TTagName>)[];
       mods?: NodeMod<TTagName>[];
-    }
+    };
 
   // Node modifier types
   export type NodeMod<TTagName extends ElementTagName = ElementTagName> =
     | Primitive
     | ExpandedElementAttributes<TTagName>
     | ExpandedElement<TTagName>;
-  export type NodeModFn<TTagName extends ElementTagName = ElementTagName> =
-    (parent: ExpandedElement<TTagName>, index: number) => NodeMod<TTagName>;
-
+  export type NodeModFn<TTagName extends ElementTagName = ElementTagName> = (
+    parent: ExpandedElement<TTagName>,
+    index: number,
+  ) => NodeMod<TTagName>;
 
   // Node tags builder type
-  export type ExpandedElementBuilder<TTagName extends ElementTagName = ElementTagName> =
-    (...rawMods: (NodeMod<TTagName> | NodeModFn<TTagName>)[]) =>
-      (parent?: ExpandedElement<TTagName>, index?: number) => ExpandedElement<TTagName>;
+  export type ExpandedElementBuilder<TTagName extends ElementTagName = ElementTagName> = (
+    ...rawMods: (NodeMod<TTagName> | NodeModFn<TTagName>)[]
+  ) => (parent?: ExpandedElement<TTagName>, index?: number) => ExpandedElement<TTagName>;
 
   // tags from tags.ts
   export const div: ExpandedElementBuilder<"div">;
   export const span: ExpandedElementBuilder<"span">;
 }
 
-export { };
+export {};
