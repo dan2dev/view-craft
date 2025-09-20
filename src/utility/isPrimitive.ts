9bc42ede15819e3524db1305e15a2abf0fb64b04
Danilo Castro <dan2dev@outlook.com>
2025-09-19 23:33:28 -0300

    update

--- src/utility/isPrimitive.ts
+++ src/utility/isPrimitive.ts
@@ -1,13 +1,12 @@
-export const isPrimitive = (
-  val: unknown
-): val is string | number | boolean | symbol | bigint | undefined | null => {
+export const isPrimitive = (val: unknown): val is string | number | boolean | symbol | bigint | undefined | null => {
   return (
     val === null ||
-    typeof val === "string" ||
-    typeof val === "number" ||
-    typeof val === "boolean" ||
-    typeof val === "symbol" ||
-    typeof val === "bigint" ||
-    typeof val === "undefined"
+    (typeof val !== "object" &&
+      (typeof val === "string" ||
+        typeof val === "number" ||
+        typeof val === "boolean" ||
+        typeof val === "symbol" ||
+        typeof val === "bigint" ||
+        typeof val === "undefined"))
   );
 };
