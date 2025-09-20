57ee7d26ee962006260a355af6cc5343779c79f2
Danilo Castro <dan2dev@outlook.com>
2025-09-20 00:59:22 -0300

    refactor: remove timing logs from global tag builders

--- src/core/registerTags.ts
+++ src/core/registerTags.ts
@@ -2,10 +2,8 @@ import { tags } from "../utility/tags";
 import { createTag } from "./elementFactory";
 
 export function registerGlobalTagBuilders(): void {
-  console.time("-----tags creation-----");
   tags.forEach((tag: ElementTagName) => {
     // @ts-ignore
     globalThis[tag] = createTag(tag);
   });
-  console.timeEnd("-----tags creation-----");
 }
